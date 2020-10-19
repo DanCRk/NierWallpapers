72f372bd2f8b1fe73be67dff5bc164c3f1efa447
DanCRk <dancrkyt@gmail.com>
2020-10-19 02:18:48 -0500

    poniendo bonito x2

--- app/src/main/java/com/wallpaper/nier/SplashActivity.java
+++ app/src/main/java/com/wallpaper/nier/SplashActivity.java
@@ -1,14 +1,29 @@
 package com.wallpaper.nier;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2500);
+
     }
 }
